60fd4cb77451ec4e2f0ad3e07e98ba6f4ead28ab
Russell Webster <russ@rwebster.co.uk>
2018-05-30 20:19:27 +0100

    1st commit

---
+++ PSTwilio/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+using System.Management.Automation;
+using System.Configuration.Install;
+using System;
+using System.Collections.ObjectModel;
+using System.Management.Automation.Runspaces;
+
+namespace PSTwilio
+{
+	[RunInstaller(true)]
+
+	public class PSTwilioSnapin : CustomPSSnapIn
+	{
+		public PSTwilioSnapin()
+          : base()
+		{
+		}
+
+		public override string Description
+		{
+			get
+			{
+				return "Powershell snap-in with cmdlets for interacting with Twilio's SMS service";
+			}
+		}
+
+		public override string Name
+		{
+			get
+			{
+				return "PSTwilio";
+			}
+		}
+
+		public override string Vendor
+		{
+			get
+			{
+				return "Russell Webster";
+			}
+		}
+
+		private Collection<CmdletConfigurationEntry> _cmdlets;
+		public override Collection<CmdletConfigurationEntry> Cmdlets
+		{
+			get
+			{
+				if (_cmdlets == null)
+				{
+					_cmdlets = new Collection<CmdletConfigurationEntry>();
+					_cmdlets.Add(new CmdletConfigurationEntry("Send-TwilioSMS", typeof(SendTwilioSMSCmdlet), "SendTwilioSMSHelp.dll-HelpText.xml"));
+				}
+
+				return _cmdlets;
+			}
+		}
+	}
+
+
+}
---
+++ PSTwilio/SendTwilioSMS.cs
@@ -0,0 +1,78 @@
+﻿/* Russell Webster <russ@rwebster.co.uk>
+ * 
+ * */
+using System.Management.Automation;
+using RestSharp;
+using System.Net;
+
+namespace PSTwilio
+{
+	[Cmdlet(VerbsCommunications.Send, "TwilioSMS")]
+	[OutputType(typeof(IRestResponse))]
+	public class SendTwilioSMSCmdlet : Cmdlet
+	{
+		[Parameter(Position=0,Mandatory = true)]
+		public string AccountSid
+		{
+			get { return _accountSid; }
+			set { _accountSid = value; }
+		}
+		private string _accountSid;
+
+		[Parameter(Position = 1, Mandatory = true)]
+		public string AuthToken
+		{
+			get { return _authToken; }
+			set { _authToken = value; }
+		}
+		private string _authToken;
+
+		[Parameter(Position = 2, Mandatory = true)]
+		public string To
+		{
+			get { return _to; }
+			set { _to = value; }
+		}
+		private string _to;
+
+		[Parameter(Position = 3, Mandatory = true)]
+		public string From
+		{
+            get { return _from; }
+			set { _from = value; }
+		}
+		private string _from;
+
+		[Parameter(Position = 4, Mandatory = true)]
+		public string MessageBody
+		{
+			get { return _messageBody; }
+			set { _messageBody = value; }
+		}
+		private string _messageBody;
+
+		private RestClient _client;
+		private RestRequest _req;
+		private IRestResponse _response;
+
+		protected override void BeginProcessing()
+		{
+			_client = new RestClient($"https://api.twilio.com/2010-04-01/Accounts/{_accountSid}/Messages.json");
+			_req = new RestRequest(Method.POST);
+		}
+
+		protected override void ProcessRecord()
+		{
+			_req.Credentials = new NetworkCredential(_accountSid, _authToken);
+			_req.AddParameter("From", _from);
+			_req.AddParameter("To", _to);
+			_req.AddParameter("Body", _messageBody);
+			_response = _client.Execute(_req);
+		}
+
+		protected override void EndProcessing()
+		{
+			WriteObject(_response);
+		}
+	}
+}
